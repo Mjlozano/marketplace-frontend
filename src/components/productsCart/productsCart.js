d98f19619c58d079ddc3979896b6909250a3514a
Rafael Velandia <ravelandia@uninorte.edu.co>
2022-06-06 17:35:25 -0500

    Se termino la vista del carro

--- src/components/productsCart/productsCart.js
+++ src/components/productsCart/productsCart.js
@@ -2,10 +2,10 @@ import React from "react";
 import { Container, Box } from "@mui/material";
 import { Button } from "@mui/material";
 
-const ProductCart = () => {
+const ProductCart = ({titulo,imagen,precio,cantidad}) => {
   return (
     <Container sx={{ width: "50%" }} className="container">
-      <h1 className="titulo">SEXOOOOOOOOOOOOOOOOOOO</h1>
+      <h1 className="titulo">{titulo}</h1>
       <div className="product-box">
         <Box
           className="image"
@@ -17,11 +17,12 @@ const ProductCart = () => {
             maxWidth: { xs: 350, md: 250 },
           }}
           alt="The house from the offer."
-          src="https://media.4rgos.it/i/Argos/9520103_R_Z001A?w=750&h=440&qlt=70"
+          src={imagen}
         />
         <div className="image-info">
-          <h1 className="precio">$ 25.000.000</h1>
-          <p className="cantidad">Cantidad: 2</p>
+          <h1 className="precio">$ {precio}</h1>
+          <p className="cantidad">Cantidad: {cantidad}</p>
+          <p className="cantidad">total por producto:${precio * cantidad}</p> 
         </div>
       </div>
     </Container>
