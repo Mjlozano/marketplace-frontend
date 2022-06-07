95aae9f39d7ab5045303836fc0cd770226e9d90b
mjlozano <mjlozanoh98@gmail.com>
2022-06-06 20:54:23 -0500

    se modificaron los productos de prueba

--- src/helpers/products.js
+++ src/helpers/products.js
@@ -1,121 +1,37 @@
 
 const products = [
     {
-        name: 'Green Iguana',
-        price: 1000,
-        category: 'Lizards',
+        name: 'Iphone 13',
+        price: 3800000,
+        category: 'Celulares y Teléfonos',
         image: 'https://media.4rgos.it/i/Argos/9520103_R_Z001A?w=750&h=440&qlt=70',
         id: 1,
-        description: 'Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging across all continents except Antarctica'
+        description: 'Iphone 13 con camara de 12MP'
     },
     {
-        name: 'Red Iguana',
-        price: 1000,
-        category: 'felines',
-        image: 'https://media.4rgos.it/i/Argos/9520103_R_Z001A?w=750&h=440&qlt=70',
+        name: 'Monitor LG',
+        price: 1500000,
+        category: 'Computación',
+        image: 'https://www.lg.com/co/images/monitores/e1942c/gallery/large01.jpg',
         id: 2,
-        description: 'Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging across all continents except Antarctica'
+        description: 'Monitor LG de 27 pulgadas'
     },
     {
-        name: 'Blue Iguana',
-        price: 1000,
-        category: 'Lizards',
-        image: 'https://media.4rgos.it/i/Argos/9520103_R_Z001A?w=750&h=440&qlt=70',
+        name: 'Smartwatch',
+        price: 700000,
+        category: 'Celulares y Teléfonos',
+        image: 'https://exitocol.vtexassets.com/arquivos/ids/9147239/smartwatch-reloj-inteligente-w26-plus-serie-6-llamada-bt-rosa.jpg?v=637629688081500000',
         id: 3,
-        description: 'Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging across all continents except Antarctica'
+        description: 'Smart Watch con pantalla de 2 pulgadas'
     },
     {
-        name: 'Yellow Iguana',
+        name: 'Samsung Galaxy S22',
         price: 1000,
-        category: 'Lizards',
-        image: 'https://media.4rgos.it/i/Argos/9520103_R_Z001A?w=750&h=440&qlt=70',
+        category: 'Celulares y Teléfonos',
+        image: 'https://falabella.scene7.com/is/image/FalabellaCO/29014956_1?wid=800&hei=800&qlt=70',
         id: 4,
-        description: 'Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging across all continents except Antarctica'
+        description: 'Samsung Galaxy S22 8gb+128gb'
     },
-    {
-        name: 'Green Iguana',
-        price: 1000,
-        category: 'Lizards',
-        image: 'https://media.4rgos.it/i/Argos/9520103_R_Z001A?w=750&h=440&qlt=70',
-        id: 5,
-        description: 'Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging across all continents except Antarctica'
-    },
-    {
-        name: 'Red Iguana',
-        price: 1000,
-        category: 'felines',
-        image: 'https://media.4rgos.it/i/Argos/9520103_R_Z001A?w=750&h=440&qlt=70',
-        id: 6,
-        description: 'Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging across all continents except Antarctica'
-    },
-    {
-        name: 'Blue Iguana',
-        price: 1000,
-        category: 'Lizards',
-        image: 'https://media.4rgos.it/i/Argos/9520103_R_Z001A?w=750&h=440&qlt=70',
-        id: 7,
-        description: 'Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging across all continents except Antarctica'
-    },
-    {
-        name: 'Yellow Iguana',
-        price: 1000,
-        category: 'felines',
-        image: 'https://media.4rgos.it/i/Argos/9520103_R_Z001A?w=750&h=440&qlt=70',
-        id: 8,
-        description: 'Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging across all continents except Antarctica'
-    },
-    {
-        name: 'Green Iguana',
-        price: 1000,
-        category: 'Lizards',
-        image: 'https://media.4rgos.it/i/Argos/9520103_R_Z001A?w=750&h=440&qlt=70',
-        id: 9,
-        description: 'Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging across all continents except Antarctica'
-    },
-    {
-        name: 'Red Iguana',
-        price: 1000,
-        category: 'felines',
-        image: 'https://media.4rgos.it/i/Argos/9520103_R_Z001A?w=750&h=440&qlt=70',
-        id: 10,
-        description: 'Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging across all continents except Antarctica'
-    },
-    {
-        name: 'Blue Iguana',
-        price: 1000,
-        category: 'felines',
-        image: 'https://media.4rgos.it/i/Argos/9520103_R_Z001A?w=750&h=440&qlt=70',
-        id: 11,
-        description: 'Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging across all continents except Antarctica'
-    },
-    {
-        name: 'Yellow Iguana',
-        price: 1000,
-        category: 'Lizards',
-        image: 'https://media.4rgos.it/i/Argos/9520103_R_Z001A?w=750&h=440&qlt=70',
-        id: 12,
-        description: 'Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging across all continents except Antarctica'
-    },{
-        name: 'Yellow Iguana',
-        price: 1000,
-        category: 'Classic',
-        image: 'https://http2.mlstatic.com/D_NQ_NP_654350-MLA46165124813_052021-V.webp',
-        id: 12,
-        description: 'Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging across all continents except Antarctica'
-    },{
-        name: 'Yellow Iguana',
-        price: 1000,
-        category: 'Classic',
-        image: 'https://http2.mlstatic.com/D_NQ_NP_654350-MLA46165124813_052021-V.webp',
-        id: 12,
-        description: 'Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging across all continents except Antarctica'
-    },{
-        name: 'Yellow Iguana',
-        price: 1000,
-        category: 'Classic',
-        image: 'https://http2.mlstatic.com/D_NQ_NP_654350-MLA46165124813_052021-V.webp',
-        id: 12,
-        description: 'Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging across all continents except Antarctica'
-    },];
+    ];
 
     export default products;
