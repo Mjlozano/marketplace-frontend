84aa29a5c9ba9022d034afdc1042724e66bdc25f
Rafael Velandia <ravelandia@uninorte.edu.co>
2022-06-07 00:48:24 -0500

    funciona la opcion de eliminar producto de la vista de administrador

--- src/views/mainPage/mainPageAdmin.js
+++ src/views/mainPage/mainPageAdmin.js
@@ -7,6 +7,7 @@ import MainPage from "./mainPage";
 
 const MainPageAdmin = () => {
   const data = JSON.parse(localStorage.getItem("stock"));
+  console.log(data);
   const columns = [
     { field: "id", headerName: "ID", width: 70 },
     { field: "name", headerName: "Nombre del producto", width: 180, editable: true },
@@ -20,7 +21,7 @@ const MainPageAdmin = () => {
     {
       field: "description",
       headerName: "Descripcion",
-      width: 300, 
+      width: 300,
       editable: true
     },
   ];
@@ -28,7 +29,34 @@ const MainPageAdmin = () => {
   function subimit() {
     window.location.href = "/new-product";
   }
-
+  var idselection;
+  function eliminar(id) {
+    idselection = id;
+  }
+  function eliminacion() {
+    var datos = JSON.parse(localStorage.getItem("stock"));
+    console.log(datos);
+    var index=[];
+    datos.forEach(element => {
+      idselection.forEach(element2 => {
+        if(element.id==element2){
+          index.push(datos.indexOf(element));
+        }
+      })
+    });
+    console.log(index);
+    var ver=true;
+    index.forEach(element => {
+      if (ver ==true) {
+        datos.splice(element, 1);
+        ver = false;
+      }else{
+        datos.splice(element-1, 1);
+      }
+    });
+    localStorage.setItem("stock", JSON.stringify(datos));
+    window.location.reload();
+  }
   return (
     <>
       <div>
@@ -45,7 +73,9 @@ const MainPageAdmin = () => {
           rowsPerPageOptions={[5]}
           checkboxSelection
           experimentalFeatures={{ newEditingApi: true }}
+          onSelectionModelChange={itm => eliminar(itm)}
         />
+        <Button variant="text" onClick={eliminacion}>Eliminar</Button>
       </div>
     </>
   );
